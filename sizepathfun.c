5228e98b2049572e9a8f78874d48ac101138ab62
Adam Wilson <adam@hoare7.cs.umsl.edu>
2020-09-08 23:33:56 -0500

    Implemented showtreesize command, fixed -c option, changed output from listing files to just total sizes

---
+++ sizepathfun.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include "sizepathfun.h"
+
+int sizepathfun(char* path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) == -1) {
+        //int err = errno;
+        //printf("-1     stat() call failed: errno = %d", err);
+        return -1;
+    }
+    else {
+	    //printf("%d", statbuf.st_blocks);
+        return statbuf.st_blocks;
+    }
+}
